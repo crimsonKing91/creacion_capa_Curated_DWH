d97f297e1e2ba993d6a9d7c52d83c41c931d8a22
crimsonKing91 <ldaniel.jrz.vrg91@gmail.com>
2026-03-09 22:43:24 -0600

    scripts CTAs

--- prueba_conn.py
+++ prueba_conn.py
@@ -3,4 +3,4 @@ try:
     response = requests.get("https://www.google.com", timeout=5)
     print(f"Conexión exitosa, status: {response.status_code}")
 except Exception as e:
-    print(f"Error general: {e}")
+    print(f"Error general: {e}") 
